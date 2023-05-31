6c6fb5a93c056c2f0e3f1d0147ac0aeb0774d71c
manuelpr07 <manuelpr@ucm.es>
2023-05-31 19:36:38 +0200

    colisiones

--- main.c
+++ main.c
@@ -6,14 +6,15 @@
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 #define INI_RADIUS 50
-#define LAG_FACTOR 0.001
+#define LAG_FACTOR 0.01
+#define MAX_PLAYERS 10
 
 typedef struct {
     float x, y;
-    int radius;
+    float radius;
     bool alive;
 } player;
-player players[11];
+player players[MAX_PLAYERS];
 //number of the player of this machine
 int playerNumber;
 
@@ -41,35 +42,51 @@ void DrawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius, Ui
     }
 }
 
-void PlayerCircle(SDL_Renderer* renderer) {
-    //update enemies pos and circle radius
-    int i = 0;
-    while (players[i].alive) {
-        if (i != playerNumber)
-            DrawCircle(renderer, players[i].x, players[i].y, players[i].radius, 255, 0, 0);
-        else
-            DrawCircle(renderer, players[i].x, players[i].y, players[i].radius, 0, 0, 255);
+// void PlayerCircle(SDL_Renderer* renderer) {
+//     //update enemies pos and circle radius
+//     for(int i = 0; i < MAX_PLAYERS; i++) {
+//         if(players[i].alive)
+//         {     
+            
+//             if (i != playerNumber)
+//                 DrawCircle(renderer, players[i].x, players[i].y, players[i].radius, 255, 0, 0);
+//             else
+//                 DrawCircle(renderer, players[i].x, players[i].y, players[i].radius, 0, 0, 255);
+//         }
+//     }
+// }
 
-        i++;
-    }
-}
+void Collisions() {
+    for(int i = 0; i < MAX_PLAYERS; i++) {
+        if (players[i].alive) {
+            for(int j = 0; j < MAX_PLAYERS; j++) {
+                if(players[j].alive){
+                    float distanceX = fabs(players[i].x - players[j].x);
+                    float distanceY = fabs(players[i].y - players[j].y);
+                    float radiusSum = players[i].radius + players[j].radius;
 
-void collitions() {
-    int i = 0;
-    while (players[i].alive) {
-        if (i != playerNumber) {
-            if (players[i].x > players[playerNumber].x - players[playerNumber].radius && players[i].x < players[playerNumber].x + players[playerNumber].radius)
-                if (players[i].y > players[playerNumber].y - players[playerNumber].radius && players[i].y < players[playerNumber].y + players[playerNumber].radius)
-                    printf("collision ");
+                    if ((distanceX <= players[i].radius && distanceY <= players[j].radius) || distanceX <= players[j].radius && distanceY <= players[i].radius) {
+                        if(players[i].radius > players[j].radius && players[j].alive)
+                        {
+                            players[j].alive = false;
+                            players[i].radius += players[j].radius/2;
+                        }
+
+                    }
+                }
+            }
         }
-        i++;
     }
 }
+
 
 void FollowPlayer(float* cameraX, float* cameraY) {
     // Adjust the camera position based on the player's position
-    *cameraX = players[playerNumber].x - WINDOW_WIDTH / 2;
-    *cameraY = players[playerNumber].y - WINDOW_HEIGHT / 2;
+    if(players[playerNumber].alive)
+    {
+        *cameraX = players[playerNumber].x - WINDOW_WIDTH / 2;
+        *cameraY = players[playerNumber].y - WINDOW_HEIGHT / 2;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -93,8 +110,8 @@ int main(int argc, char* argv[]) {
     insertPlayer(WINDOW_WIDTH / 2, WINDOW_WIDTH / 2, INI_RADIUS);
 
     //inserting example enemies
-    insertPlayer(WINDOW_WIDTH / 3, WINDOW_WIDTH / 5, 60);
-    insertPlayer(WINDOW_WIDTH / 5, WINDOW_WIDTH / 5, 20);
+    insertPlayer(WINDOW_WIDTH / 4, WINDOW_WIDTH /4, 40);
+    insertPlayer(WINDOW_WIDTH / 4, WINDOW_WIDTH /4, 30);
 
     // Camera position
     float cameraX = 0;
@@ -130,19 +147,23 @@ int main(int argc, char* argv[]) {
             players[playerNumber].y += (mouseY - WINDOW_HEIGHT/ 2 ) * LAG_FACTOR;
 
         FollowPlayer(&cameraX, &cameraY); // Adjust the camera position
+        Collisions();
 
         // Render the players with the camera offset
         int i = 0;
-        while (players[i].alive) {
-            float renderX = players[i].x - cameraX;
-            float renderY = players[i].y - cameraY;
+        for(int i = 0; i < MAX_PLAYERS; i++) {
+            if(players[i].alive)
+            {
 
-            if (i != playerNumber)
-                DrawCircle(renderer, renderX, renderY, players[i].radius, 255, 0, 0);
-            else
-                DrawCircle(renderer, renderX, renderY, players[i].radius, 0, 0, 255);
+                float renderX = players[i].x - cameraX;
+                float renderY = players[i].y - cameraY;
 
-            i++;
+                if (i != playerNumber)
+                    DrawCircle(renderer, renderX, renderY, players[i].radius, 255, 0, 0);
+                else
+                    DrawCircle(renderer, renderX, renderY, players[i].radius, 0, 0, 255);
+            }
+
         }
 
         SDL_RenderPresent(renderer);
@@ -150,6 +171,5 @@ int main(int argc, char* argv[]) {
 
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
