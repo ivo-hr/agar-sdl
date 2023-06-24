b1790e3665e28ac2afa05017ef789081adb39353
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2023-06-24 14:36:50 +0200

    Update Message.cpp

--- net/Message.cpp
+++ net/Message.cpp
@@ -71,3 +71,200 @@ int LoginMessage::from_bin(char * src){
 }
 
 // PositionMessage -------------------------------------------------------------
+
+PositionMessage::PositionMessage() : Message(POSITIONS), size(0) {
+    MSG_SIZE = sizeof(size_t);
+}
+
+PositionMessage::PositionMessage(std::vector<std::pair<float, float>> pPos) : Message(POSITIONS), size(pPos.size()) {
+    MSG_SIZE = sizeof(size_t) + sizeof(float) * 2 * size;
+    pos = pPos;
+}
+
+void PositionMessage::to_bin(){
+    alloc_data(MAX_SIZE);
+    memset(_data, 0, MSG_SIZE);
+
+    char * tmp = _data;
+    memcpy(tmp, &type, sizeof(MessageType));
+    tmp += sizeof(MessageType);
+
+    memcpy(tmp, &size, sizeof(size_t));
+    tmp += sizeof(size_t);
+
+    for (auto &p : pos) {
+        memcpy(tmp, &p.first, sizeof(float));
+        tmp += sizeof(float);
+        memcpy(tmp, &p.second, sizeof(float));
+        tmp += sizeof(float);
+    }
+}
+
+int PositionMessage::from_bin(char * src){
+    alloc_data(MAX_SIZE);
+    char * tmp = src;
+    memcpy(static_cast<void*>(_data), tmp, MAX_SIZE);
+    memcpy(&type, tmp, sizeof(MessageType));
+    tmp += sizeof(MessageType);
+
+    memcpy(&size, tmp, sizeof(size_t));
+    tmp += sizeof(size_t);
+
+    for (size_t i = 0; i < size; ++i) {
+        float x, y;
+        memcpy(&x, tmp, sizeof(float));
+        tmp += sizeof(float);
+        memcpy(&y, tmp, sizeof(float));
+        tmp += sizeof(float);
+        pos.push_back(std::make_pair(x, y));
+    }
+
+    return 0;
+}
+
+// SizeMessage ----------------------------------------------------------------
+
+SizeMessage::SizeMessage() : Message(SIZES), size(0) {
+    MSG_SIZE = sizeof(size_t);
+}
+
+SizeMessage::SizeMessage(std::vector<int> pSize) : Message(SIZES), size(pSize.size()) {
+    MSG_SIZE = sizeof(size_t) + sizeof(float) * size;
+    size_ = pSize;
+}
+
+void SizeMessage::to_bin(){
+    alloc_data(MAX_SIZE);
+    memset(_data, 0, MSG_SIZE);
+
+    char * tmp = _data;
+    memcpy(tmp, &type, sizeof(MessageType));
+    tmp += sizeof(MessageType);
+
+    memcpy(tmp, &size, sizeof(size_t));
+    tmp += sizeof(size_t);
+
+    for (auto &s : size_) {
+        memcpy(tmp, &s, sizeof(float));
+        tmp += sizeof(float);
+    }
+
+}
+
+int SizeMessage::from_bin(char * src){
+    alloc_data(MAX_SIZE);
+    char * tmp = src;
+    memcpy(static_cast<void*>(_data), tmp, MAX_SIZE);
+    memcpy(&type, tmp, sizeof(MessageType));
+    tmp += sizeof(MessageType);
+
+    memcpy(&size, tmp, sizeof(size_t));
+    tmp += sizeof(size_t);
+
+    for (size_t i = 0; i < size; ++i) {
+        float s;
+        memcpy(&s, tmp, sizeof(float));
+        tmp += sizeof(float);
+        size_.push_back(s);
+    }
+
+    return 0;
+}
+
+// FoodMessage ----------------------------------------------------------------
+
+FoodMessage::FoodMessage() : Message(FOOD), size(0) {
+    MSG_SIZE = sizeof(size_t);
+}
+
+FoodMessage::FoodMessage(std::vector<std::pair<float, float>> pFood) : Message(FOOD), size(pFood.size()) {
+    MSG_SIZE = sizeof(size_t) + sizeof(float) * 2 * size;
+    food = pFood;
+}
+
+void FoodMessage::to_bin(){
+    alloc_data(MAX_SIZE);
+    memset(_data, 0, MSG_SIZE);
+
+    char * tmp = _data;
+    memcpy(tmp, &type, sizeof(MessageType));
+    tmp += sizeof(MessageType);
+
+    memcpy(tmp, &size, sizeof(size_t));
+    tmp += sizeof(size_t);
+
+    for (auto &p : food) {
+        memcpy(tmp, &p.first, sizeof(float));
+        tmp += sizeof(float);
+        memcpy(tmp, &p.second, sizeof(float));
+        tmp += sizeof(float);
+    }
+}
+
+int FoodMessage::from_bin(char * src){
+    alloc_data(MAX_SIZE);
+    char * tmp = src;
+    memcpy(static_cast<void*>(_data), tmp, MAX_SIZE);
+    memcpy(&type, tmp, sizeof(MessageType));
+    tmp += sizeof(MessageType);
+
+    memcpy(&size, tmp, sizeof(size_t));
+    tmp += sizeof(size_t);
+
+    for (size_t i = 0; i < size; ++i) {
+        float x, y;
+        memcpy(&x, tmp, sizeof(float));
+        tmp += sizeof(float);
+        memcpy(&y, tmp, sizeof(float));
+        tmp += sizeof(float);
+        food.push_back(std::make_pair(x, y));
+    }
+
+    return 0;
+}
+
+// InputMessage ---------------------------------------------------------------
+
+InputMessage::InputMessage() : Message(INPUT), size(0) {
+    MSG_SIZE = sizeof(size_t);
+}
+
+InputMessage::InputMessage(std::pair<float, float> pInput) : Message(INPUT), size(pInput.size()) {
+    MSG_SIZE = sizeof(size_t) + sizeof(float) * 2 * size;
+    input = pInput;
+}
+
+void InputMessage::to_bin(){
+    alloc_data(MAX_SIZE);
+    memset(_data, 0, MSG_SIZE);
+
+    char * tmp = _data;
+    memcpy(tmp, &type, sizeof(MessageType));
+    tmp += sizeof(MessageType);
+
+    memcpy(tmp, &size, sizeof(size_t));
+    tmp += sizeof(size_t);
+
+    memcpy(tmp, &input.first, sizeof(float));
+    tmp += sizeof(float);
+    memcpy(tmp, &input.second, sizeof(float));
+    tmp += sizeof(float);
+}
+
+int InputMessage::from_bin(char * src){
+    alloc_data(MAX_SIZE);
+    char * tmp = src;
+    memcpy(static_cast<void*>(_data), tmp, MAX_SIZE);
+    memcpy(&type, tmp, sizeof(MessageType));
+    tmp += sizeof(MessageType);
+
+    memcpy(&size, tmp, sizeof(size_t));
+    tmp += sizeof(size_t);
+
+    memcpy(&input.first, tmp, sizeof(float));
+    tmp += sizeof(float);
+    memcpy(&input.second, tmp, sizeof(float));
+    tmp += sizeof(float);
+
+    return 0;
+}
